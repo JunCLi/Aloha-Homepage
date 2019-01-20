8eb5734db9fdc9476966b22807b6a7ecaea75abf
Kevin JC Li <kevinjcli1994@gmail.com>
2019-01-20 17:25:42 -0500

    finished adding basic flickity functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+(function($, window, document) {
+  $('.favourite-products ul').flickity({
+    wrapAround: true,
+    autoPlay: 4000,
+    imagesLoaded: true
+  });
+
+
+}(window.jQuery, window, document));
