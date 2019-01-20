87f401e1190a2fabb2293a17a67f11cfe4058358
Kevin JC Li <kevinjcli1994@gmail.com>
2019-01-20 18:12:39 -0500

    added email validation to form submission

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 (function($, window, document) {
+
+  const validateEmail = (email) => {
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+  };
+
+
+  $('#subscribe-email').on('click', event => {
+    event.preventDefault();
+    const inputtedEmail = $(event.currentTarget).siblings().val();
+    if (validateEmail(inputtedEmail)) {
+      alert('Thanks for subscribing!');
+    } else {
+      alert('Please enter a valid email address');
+    }
+    
+  });
+
+
+
+
+
   $('.favourite-products ul').flickity({
     wrapAround: true,
     autoPlay: 4000,
     imagesLoaded: true
   });
-
 
 }(window.jQuery, window, document));
