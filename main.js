0e6eff6f2b96b8b5c8ee25fb0e30a0a4e1fd685a
Kevin JC Li <kevinjcli1994@gmail.com>
2019-01-21 10:20:25 -0500

    added add to cart button in faovurite products

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 (function($, window, document) {
 
+  
+
   const validateEmail = (email) => {
     const $emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return $emailRegex.test(email);
@@ -18,7 +20,7 @@
 
   $('.favourite-products ul').flickity({
     wrapAround: true,
-    autoPlay: 4000,
+    // autoPlay: 4000,
     imagesLoaded: true
   });
 
