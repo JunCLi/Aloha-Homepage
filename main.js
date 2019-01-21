d792fe56aa929c0168e5da6ab8d686f8b03c482e
Kevin JC Li <kevinjcli1994@gmail.com>
2019-01-21 11:57:09 -0500

    got add to cart button to update number in cart working

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 (function($, window, document) {
 
-  
+  const tractItemsInCart = () => {
+    let $itemsInCart = $('.items-in-cart');
+    updateNumberOfItems = parseInt($itemsInCart.html(), 10) + 1;
+    $itemsInCart.html(updateNumberOfItems);
+  };
+
+  $('.add-to-cart').on('click', event => {
+    tractItemsInCart();
+  });
+
 
   const validateEmail = (email) => {
-    const $emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return $emailRegex.test(email);
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
   };
 
   $('#subscribe-email').on('click', event => {
